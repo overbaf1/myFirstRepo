4ad90d202c45bdaaf788e2e894b039a11093df53
Timur777999 <114388626+Timur777999@users.noreply.github.com>
2023-01-12 18:55:19 +0300

    Мой первый коммит дубль 3

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello world") // строка
+console.log(666) // число 
+console.log(66+66   )
+console.log('Hello ' + 'WORLD')
+console.log("Hello " + "WORLD");
+console.log("Hello " + "WORLD");
